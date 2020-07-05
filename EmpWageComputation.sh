4f9ba1d3c5039cbc591c4dd3c65bf6bd62a3da08
Saad <skamil60@gmail.com>
2020-07-05 14:24:28 +0530

    Stored : Daily Wage And Total Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,9 +1,11 @@
 echo "Welcome to the employee wage program."
 
+declare -A dailywage
+declare -A totalwage
 WAGE_PER_HOUR=20
 WORKING_DAYS_PER_MONTH=20
 TOTAL_WORKING_HOURS=100
-PRESENT_HOURS=1
+PRESENT_HOURS=0
 DAY=1
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
@@ -12,27 +14,36 @@ FULL_TIME=1
 
 function getHours(){
 
-	local randomCheck=$1
-	case $randomCheck in
-		$FULL_TIME)
-			echo $FULL_DAY_HOUR;;
-			
-		$PART_TIME)
-			echo $PART_TIME_HOUR;;
-			
-		*)echo 0;;
-		
-	esac
+        local randomCheck=$1
+        case $randomCheck in
+                $FULL_TIME)
+                        echo $FULL_DAY_HOUR;;
+
+                $PART_TIME)
+                        echo $PART_TIME_HOUR;;
+
+                *)echo 0;;
+
+        esac
 }
 
 while [ $DAY -le $WORKING_DAYS_PER_MONTH ] && [ $PRESENT_HOURS -lt $TOTAL_WORKING_HOURS ]
 do
 
-	randomCheck=$((RANDOM%3))
-	DAILY_HOURS="$( getHours $randomCheck )"
-	DAILY_WAGE=$(($WAGE_PER_HOUR * $DAILY_HOURS))
-        MONTHLY_WAGE=$(($WORKING_DAYS_PER_MONTH * $DAILY_WAGE))
-        echo "Monthly wage of the employee is : $MONTHLY_WAGE"
+        randomCheck=$((RANDOM%3))
+        DAILY_HOURS="$( getHours $randomCheck )"
+        dailywage["Day $DAY"]=$(($WAGE_PER_HOUR * $DAILY_HOURS))
+        PRESENT_HOURS=$(( $PRESENT_HOURS + $DAILY_HOURS ))
+        totalwage["Day $DAY"]=$(($PRESENT_HOURS * $WAGE_PER_HOUR))
 
-((DAY++))
+        ((DAY++))
+done
+
+echo "Daily wage	Total wage "
+
+for (( i=1;i<$DAY;i++ ))
+do
+
+        echo "${dailywage[Day $i]}		${totalwage[Day $i]}"
+
 done
